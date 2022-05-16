42328c3c73888c25db7e69b43b63dc2a7679e5ee
Bavo Lesy <bavo.lesy@student.uantwerpen.be>
2022-05-16 09:07:35 +0200

    trying update

--- src/main/java/Node/FileManager.java
+++ src/main/java/Node/FileManager.java
@@ -30,7 +30,8 @@ public class FileManager extends Thread {
         this.node = node;
         this.sendFiles = true;
         String launchDirectory = System.getProperty("user.dir");
-        this.localFolder = new File(launchDirectory + "/src/main/resources/LocalFiles");
+        System.out.println(launchDirectory);
+        this.localFolder = new File(launchDirectory + "/src/main/resources/LocalFiles"); //All localfiles
         System.out.println(this.localFolder);
         this.replicatedFolder = new File( launchDirectory + "/src/main/resources/ReplicatedFiles");
         this.localFiles = this.localFolder.listFiles();
@@ -38,24 +39,25 @@ public class FileManager extends Thread {
     }
 
     public void run(){
-        while(this.node.discoveryNode.getNode().getRunning()) {
+        //Starting the FileManager
+        while(this.node.discoveryNode.getNode().getRunning()) {  //while the node is running
             while(this.sendFiles) {
                 //System.out.println(this.node.discoveryNode.isDiscoveryPhase());
-                if (!this.node.discoveryNode.isDiscoveryPhase()) {
+                if (!this.node.discoveryNode.isDiscoveryPhase()) { //if the node is out of the discovery phase
                     System.out.println(Arrays.toString(this.localFiles));
-                    for (File f : this.localFiles) {
-                        System.out.println(f.getName());
-                        String fileLocation = this.node.getFile(f.getName());
+                    for (File f : this.localFiles) { // for every local File
+                        System.out.println(f.getName()); //print out the name
+                        String fileLocation = this.node.getFile(f.getName()); //get the location where the file should be
                         if (!fileLocation.equals("Error")) {
                             JSONParser parser = new JSONParser();
                             try {
                                 Object obj = parser.parse(fileLocation);
-                                int locationID = (int) (long) ((JSONObject) obj).get("node ID");
+                                int locationID = (int) (long) ((JSONObject) obj).get("node ID"); // get ID where the file should be
                                 System.out.println("local" + InetAddress.getLocalHost().getHostAddress());
-                                String locationIP = ((JSONObject) obj).get("node IP").toString();
-                                if(!locationIP.equals(InetAddress.getLocalHost().getHostAddress())) {
-                                    sendFile(f, locationIP);
-                                    this.sharedFiles.put(f.getName(), locationIP);
+                                String locationIP = ((JSONObject) obj).get("node IP").toString(); // get IP where the file should be
+                                if(!locationIP.equals(InetAddress.getLocalHost().getHostAddress())) { // if the file should be transferred
+                                    sendFile(f, locationIP); //sendFile to the right node
+                                    this.sharedFiles.put(f.getName(), locationIP); //keep track of which files are shared
                                 }
                                 System.out.println("succes send");
                                 System.out.println(this.sharedFiles);
@@ -69,11 +71,11 @@ public class FileManager extends Thread {
                 }
             }
         try(ServerSocket receivingSocket = new ServerSocket(5000)){ // Try connecting to port 5000 to start listening to clients
-            while(!this.sendFiles) {
-                Socket sendingSocket = receivingSocket.accept();
+            while(!this.sendFiles) { //while we are not sending anymore
+                Socket sendingSocket = receivingSocket.accept(); //try accepting sockets
                 dataInputStream = new DataInputStream(sendingSocket.getInputStream());
                 System.out.println(sendingSocket + " connected.");
-                receiveFile(this.replicatedFolder.toString());
+                receiveFile(this.replicatedFolder.toString()); //receive the file
                 //receivingSocket.close();
             }
         } catch (Exception e){
@@ -81,6 +83,10 @@ public class FileManager extends Thread {
         }
         }
     }
+
+
+
+    //Handling receive & send of files
     private void receiveFile(String path) throws Exception{
         int bytes = 0;
         String fileName = dataInputStream.readUTF();
