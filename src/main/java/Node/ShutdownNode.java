cbb19d5e04a345d3f8d6780607a2ee997c308356
Bavo Lesy <bavo.lesy@student.uantwerpen.be>
2022-05-23 10:34:56 +0200

    trying update

--- src/main/java/Node/ShutdownNode.java
+++ src/main/java/Node/ShutdownNode.java
@@ -29,7 +29,7 @@ public class ShutdownNode extends Thread{
         this.previousIP = node.discoveryNode.getPreviousIP();
         this.shutdownSocket = new DatagramSocket(8002);
         this.shutdownSocket.setSoTimeout(1000);
-        this.sharedfiles = FileManager.getSentFiles();
+        this.sharedfiles = FileSend.getSentFiles();
     }
     @Override
     public void start(){
